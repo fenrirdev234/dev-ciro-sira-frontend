93280b0c61031e6d23913241350c083646d307d7
Ciro S <ciroalejandro1@gmail.com>
2025-02-20 22:32:05 -0400

    feat: add custom style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,26 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				"proyect-green": "#D8F34E",
+				"proyect-black": "#000000",
+				"proyect-black2": "#240F35",
+				"proyect-white": "#FFFFFF",
+				"proyect-white2": "#FEFDF9",
+				"proyect-grey": "#8C8C8C",
+				"proyect-grey2": "#F6F6F6",
+				"proyect-red": "#FF2F2F",
+				"proyect-purple": "#B97BE6",
+				"proyect-purple2": "#9C73F7",
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
