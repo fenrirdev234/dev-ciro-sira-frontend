09d96c277096053601cbcf2aac2c876147d32959
Ciro S <ciroalejandro1@gmail.com>
2025-03-03 03:02:16 -0400

    fix: minor fix

--- src/components/ui/form/FormModal.tsx
+++ src/components/ui/form/FormModal.tsx
@@ -88,7 +88,10 @@ export const FormModal = () => {
 
 	const onError = (error: FieldErrors<PostFormType>) => {
 		error.title && toast.error(error.title?.message);
-		error.image && toast.error(error.image?.message);
+		error.image &&
+			toast.error(
+				typeof error.image?.message === "string" && error.image?.message,
+			);
 	};
 
 	return (
