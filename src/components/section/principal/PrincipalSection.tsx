2d2e0f8e68122488d4152554d8007527f0df620c
Ciro S <ciroalejandro1@gmail.com>
2025-03-03 02:43:56 -0400

    feat: add pagination and fix the grid

--- src/components/section/principal/PrincipalSection.tsx
+++ src/components/section/principal/PrincipalSection.tsx
@@ -3,24 +3,19 @@
 import { Card } from "@/components/ui/cards/Card";
 
 import { Subscribe } from "../subscribe/Subscribe";
-import { IPost, IPostPagination } from "@/models/post.model";
+import { IPostPagination } from "@/models/post.model";
 
-import { usePathname, useSearchParams } from "next/navigation";
-import { useRouter } from "next/router";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
 import { useDebouncedCallback } from "use-debounce";
 import { motion } from "motion/react";
+import { Button } from "../../ui/buttons/Button";
 
-export const PrincipalSection = ({
-	currentPage = 1,
-	posts,
-}: {
-	currentPage: number;
-	posts: IPost[];
-}) => {
-	/* const searchParams = useSearchParams();
+export const PrincipalSection = ({ postAll }: { postAll: IPostPagination }) => {
+	const searchParams = useSearchParams();
 	const pathname = usePathname();
 	const { replace } = useRouter();
-
+	const posts = postAll.docs;
 	const handleLoad = useDebouncedCallback((page: string) => {
 		const params = new URLSearchParams(searchParams);
 		if (page) {
@@ -28,8 +23,8 @@ export const PrincipalSection = ({
 		} else {
 			params.delete(page);
 		}
-		replace(`${pathname.toString()}`);
-	}, 400); */
+		replace(`${pathname}?${params.toString()}`);
+	}, 400);
 
 	const myAnimationLeft = {
 		initial: { opacity: 0, x: -40 },
@@ -41,179 +36,199 @@ export const PrincipalSection = ({
 	};
 
 	return (
-		<div className='lx:max-w-[974px] flex w-full flex-col gap-8 md:gap-14'>
+		<section className='lx:max-w-[974px] flex w-full flex-col items-center gap-8 pb-[56px] md:gap-14 lg:pb-[98px]'>
 			{posts[0] && (
-				<div className='grid grid-cols-1 grid-rows-1 gap-8 md:grid-cols-3 md:grid-rows-2'>
-					<motion.div
-						initial='initial'
-						whileInView='inView'
-						viewport={{ once: true }}
-						variants={myAnimationLeft}
-						className='flex aspect-[327/378] md:col-span-2 md:row-span-2 md:aspect-[264/395]'
-					>
-						<Card
-							title={posts[0].title}
-							category={posts[0].category}
-							postImage={posts[0].postImage}
-							readingTime={posts[0].readingTime}
-							isPrincipal={false}
-							postId={posts[0].postId}
-						/>
-					</motion.div>
-					{posts[1] && (
+				<div className='flex w-full flex-col gap-8 sm:aspect-[974/910] sm:flex-row'>
+					<div className='flex w-full sm:w-[55%]'>
 						<motion.div
 							initial='initial'
 							whileInView='inView'
 							viewport={{ once: true }}
-							variants={myAnimationRigth}
-							className='flex aspect-[327/378] md:col-start-3 md:aspect-auto'
+							variants={myAnimationLeft}
+							className='flex aspect-[327/378] flex-1 sm:aspect-auto'
 						>
 							<Card
-								title={posts[1].title}
-								category={posts[1].category}
-								postImage={posts[1].postImage}
-								readingTime={posts[1].readingTime}
+								title={posts[0].title}
+								category={posts[0].category}
+								postImage={posts[0].postImage}
+								readingTime={posts[0].readingTime}
 								isPrincipal={false}
-								postId={posts[1].postId}
+								postId={posts[0].postId}
 							/>
 						</motion.div>
-					)}
-
-					{posts[2] && (
-						<motion.div
-							initial='initial'
-							whileInView='inView'
-							viewport={{ once: true }}
-							variants={myAnimationRigth}
-							className='flex aspect-[327/378] md:col-start-3 md:row-start-2 md:aspect-auto'
-						>
-							<Card
-								title={posts[2].title}
-								category={posts[2].category}
-								postImage={posts[2].postImage}
-								readingTime={posts[2].readingTime}
-								isPrincipal={false}
-								postId={posts[2].postId}
-							/>
-						</motion.div>
-					)}
+					</div>
+					<div className='flex aspect-[327/378] w-full flex-col gap-8 sm:w-[45%]'>
+						{posts[1] && (
+							<motion.div
+								initial='initial'
+								whileInView='inView'
+								viewport={{ once: true }}
+								variants={myAnimationRigth}
+								className='flex aspect-[327/378] flex-1 sm:aspect-auto'
+							>
+								<Card
+									title={posts[1].title}
+									category={posts[1].category}
+									postImage={posts[1].postImage}
+									readingTime={posts[1].readingTime}
+									isPrincipal={false}
+									postId={posts[1].postId}
+								/>
+							</motion.div>
+						)}
+						{posts[2] && (
+							<motion.div
+								initial='initial'
+								whileInView='inView'
+								viewport={{ once: true }}
+								variants={myAnimationRigth}
+								className='flex aspect-[327/378] flex-1 sm:aspect-auto'
+							>
+								<Card
+									title={posts[2].title}
+									category={posts[2].category}
+									postImage={posts[2].postImage}
+									readingTime={posts[2].readingTime}
+									isPrincipal={false}
+									postId={posts[2].postId}
+								/>
+							</motion.div>
+						)}
+					</div>
 				</div>
 			)}
+
 			<Subscribe />
 
 			{posts[3] && (
-				<div className='grid grid-cols-1 grid-rows-1 gap-8 md:grid-cols-3 md:grid-rows-2'>
-					<motion.div
-						initial='initial'
-						whileInView='inView'
-						viewport={{ once: true }}
-						variants={myAnimationRigth}
-						className='flex aspect-[327/378] md:col-span-2 md:col-start-2 md:row-span-2 md:row-start-1 md:aspect-[264/395]'
-					>
-						<Card
-							title={posts[3].title}
-							category={posts[3].category}
-							postImage={posts[3].postImage}
-							readingTime={posts[3].readingTime}
-							isPrincipal={false}
-							postId={posts[3].postId}
-						/>
-					</motion.div>
-					{posts[4] && (
+				<div className='flex w-full flex-col gap-8 sm:aspect-[974/910] sm:flex-row'>
+					<div className='flex aspect-[327/378] w-full flex-col gap-8 sm:w-[45%]'>
 						<motion.div
 							initial='initial'
 							whileInView='inView'
 							viewport={{ once: true }}
 							variants={myAnimationLeft}
-							className='flex aspect-[327/378] md:col-start-1 md:row-start-1 md:aspect-auto'
+							className='flex aspect-[327/378] flex-1 sm:aspect-auto'
 						>
 							<Card
-								title={posts[4].title}
-								category={posts[4].category}
-								postImage={posts[4].postImage}
-								readingTime={posts[4].readingTime}
+								title={posts[3].title}
+								category={posts[3].category}
+								postImage={posts[3].postImage}
+								readingTime={posts[3].readingTime}
 								isPrincipal={false}
-								postId={posts[4].postId}
+								postId={posts[3].postId}
 							/>
 						</motion.div>
-					)}
+
+						{posts[4] && (
+							<motion.div
+								initial='initial'
+								whileInView='inView'
+								viewport={{ once: true }}
+								variants={myAnimationLeft}
+								className='flex aspect-[327/378] flex-1 sm:aspect-auto'
+							>
+								<Card
+									title={posts[4].title}
+									category={posts[4].category}
+									postImage={posts[4].postImage}
+									readingTime={posts[4].readingTime}
+									isPrincipal={false}
+									postId={posts[4].postId}
+								/>
+							</motion.div>
+						)}
+					</div>
 					{posts[5] && (
-						<motion.div
-							initial='initial'
-							whileInView='inView'
-							viewport={{ once: true }}
-							variants={myAnimationLeft}
-							className='flex aspect-[327/378] md:row-start-2 md:aspect-auto'
-						>
-							<Card
-								title={posts[5].title}
-								category={posts[5].category}
-								postImage={posts[5].postImage}
-								readingTime={posts[5].readingTime}
-								isPrincipal={false}
-								postId={posts[5].postId}
-							/>
-						</motion.div>
+						<div className='flex w-full sm:w-[55%]'>
+							<motion.div
+								initial='initial'
+								whileInView='inView'
+								viewport={{ once: true }}
+								variants={myAnimationRigth}
+								className='flex aspect-[327/378] flex-1 sm:aspect-auto'
+							>
+								<Card
+									title={posts[5].title}
+									category={posts[5].category}
+									postImage={posts[5].postImage}
+									readingTime={posts[5].readingTime}
+									isPrincipal={false}
+									postId={posts[5].postId}
+								/>
+							</motion.div>
+						</div>
 					)}
 				</div>
 			)}
 
 			{posts[6] && (
-				<div className='grid grid-cols-1 grid-rows-1 gap-8 md:grid-cols-3 md:grid-rows-2'>
-					<motion.div
-						initial='initial'
-						whileInView='inView'
-						viewport={{ once: true }}
-						variants={myAnimationLeft}
-						className='flex aspect-[327/378] md:col-span-2 md:row-span-2 md:aspect-[264/395]'
-					>
-						<Card
-							title={posts[6].title}
-							category={posts[6].category}
-							postImage={posts[6].postImage}
-							readingTime={posts[6].readingTime}
-							isPrincipal={false}
-							postId={posts[6].postId}
-						/>
-					</motion.div>
-					{posts[7] && (
+				<div className='flex w-full flex-col gap-8 sm:aspect-[974/910] sm:flex-row'>
+					<div className='flex w-full sm:w-[55%]'>
 						<motion.div
 							initial='initial'
 							whileInView='inView'
 							viewport={{ once: true }}
-							variants={myAnimationRigth}
-							className='flex aspect-[327/378] md:col-start-3 md:aspect-auto'
+							variants={myAnimationLeft}
+							className='flex aspect-[327/378] flex-1 sm:aspect-auto'
 						>
 							<Card
-								title={posts[7].title}
-								category={posts[7].category}
-								postImage={posts[7].postImage}
-								readingTime={posts[7].readingTime}
+								title={posts[6].title}
+								category={posts[6].category}
+								postImage={posts[6].postImage}
+								readingTime={posts[6].readingTime}
 								isPrincipal={false}
-								postId={posts[7].postId}
+								postId={posts[6].postId}
 							/>
 						</motion.div>
-					)}
-					{posts[8] && (
-						<motion.div
-							className='flex aspect-[327/378] md:col-start-3 md:row-start-2 md:aspect-auto'
-							initial='initial'
-							whileInView='inView'
-							viewport={{ once: true }}
-							variants={myAnimationRigth}
-						>
-							<Card
-								title={posts[8].title}
-								category={posts[8].category}
-								postImage={posts[8].postImage}
-								readingTime={posts[8].readingTime}
-								isPrincipal={false}
-								postId={posts[8].postId}
-							/>
-						</motion.div>
-					)}
+					</div>
+					<div className='flex aspect-[327/378] w-full flex-col gap-8 sm:w-[45%]'>
+						{posts[7] && (
+							<motion.div
+								initial='initial'
+								whileInView='inView'
+								viewport={{ once: true }}
+								variants={myAnimationRigth}
+								className='flex aspect-[327/378] flex-1 sm:aspect-auto'
+							>
+								<Card
+									title={posts[7].title}
+									category={posts[7].category}
+									postImage={posts[7].postImage}
+									readingTime={posts[7].readingTime}
+									isPrincipal={false}
+									postId={posts[7].postId}
+								/>
+							</motion.div>
+						)}
+						{posts[8] && (
+							<motion.div
+								initial='initial'
+								whileInView='inView'
+								viewport={{ once: true }}
+								variants={myAnimationRigth}
+								className='flex aspect-[327/378] flex-1 sm:aspect-auto'
+							>
+								<Card
+									title={posts[8].title}
+									category={posts[8].category}
+									postImage={posts[8].postImage}
+									readingTime={posts[8].readingTime}
+									isPrincipal={false}
+									postId={posts[8].postId}
+								/>
+							</motion.div>
+						)}
+					</div>
 				</div>
 			)}
-		</div>
+
+			{postAll.hasNextPage && (
+				<Button
+					label='Load more'
+					onClick={() => handleLoad(postAll.nextPage.toString())}
+				/>
+			)}
+		</section>
 	);
 };
